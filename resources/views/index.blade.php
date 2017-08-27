acc0c9c3fe62bb081963623ea65c16323cbbe306
Giovanny Avila <gjavila1995@gmail.com>
2017-08-28 01:13:08 +0200

    SE ELIMINO MAS PLUGINS JS INNECESARIOS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -191,20 +191,11 @@
     <!-- Bootstrap Core Js -->
     <script src="{{ asset('plugins/bootstrap/js/bootstrap.js') }}"></script>
 
-    <!-- Select Plugin Js -->
-    <script src="{{ asset('plugins/bootstrap-select/js/bootstrap-select.js') }}"></script>
-
-    <!-- Slimscroll Plugin Js -->
-    <script src="{{ asset('plugins/jquery-slimscroll/jquery.slimscroll.js') }}"></script>
-
     <!-- Waves Effect Plugin Js -->
     <script src="{{ asset('plugins/node-waves/waves.js') }}"></script>
 
     <!-- Jquery CountTo Plugin Js -->
     <script src="{{ asset('plugins/jquery-countto/jquery.countTo.js') }}"></script>
-
-    <!-- Morris Plugin Js -->
-    <script src="{{ asset('plugins/raphael/raphael.min.js') }}"></script>
     <script src="{{ asset('plugins/morrisjs/morris.js') }}"></script>
 
 
