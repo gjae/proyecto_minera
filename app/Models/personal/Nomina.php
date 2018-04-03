af0982121a58ad6b43695b75b70083a7affdae1c
Giovanny Avila <gjavila1995@gmail.com>
2018-04-03 17:01:11 +0200

    ARREGLADO EL SOFTDELETES PARA LAS NOMINAS

--- app/Models/personal/Nomina.php
+++ app/Models/personal/Nomina.php
@@ -3,20 +3,22 @@ namespace App\Models\personal;
 
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
 * 
 */
 class Nomina extends Model
 {
+	use SoftDeletes;
 	protected $table = 'nominas';
 	protected $fillable = [
 		'codigo_nomina', 'periodo_nomina', 'estado_nomina', 'total_nomina', 'total_deducciones', 'nombre_nomina', 'tipo_nomina', 
 	];
-
 	protected $casts = [
 		'periodo_nomina' => 'date'
 	];
+	protected $dates = ['deleted_at'];
 
 
 	public function detalles(){
